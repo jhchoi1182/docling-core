d23b2e2556d47052e9dc8c95deb5cd2cfb49ea30
Cesar Berrospi Ramis <ceb@zurich.ibm.com>
2025-12-12 11:04:59 +0100

    tests(webvtt): add test for ProvenanceTrack
    Signed-off-by: Cesar Berrospi Ramis <ceb@zurich.ibm.com>

--- test/test_doc_base.py
+++ test/test_doc_base.py
@@ -1,6 +1,8 @@
 import pytest
 from pydantic import ValidationError
 
+from docling_core.types.doc.document import ProvenanceTrack
+from docling_core.types.doc.webvtt import WebVTTTimestamp
 from docling_core.types.legacy_doc.base import Prov, S3Reference
 
 
@@ -37,3 +39,34 @@ def test_prov():
     with pytest.raises(ValidationError, match="at least 2 items"):
         prov["span"] = [0]
         Prov(**prov)
+
+
+def test_prov_track():
+    """Test the class ProvenanceTrack."""
+
+    valid_track = ProvenanceTrack(
+        start_time=WebVTTTimestamp(raw="00:11.000"),
+        end_time=WebVTTTimestamp(raw="00:12.000"),
+        identifier="test",
+        voice="Mary",
+        languages=["en", "en-GB"],
+        classes=["v.first.loud", "i.foreignphrase"],
+    )
+
+    assert valid_track
+    assert valid_track.start_time == WebVTTTimestamp(raw="00:11.000")
+    assert valid_track.end_time == WebVTTTimestamp(raw="00:12.000")
+    assert valid_track.identifier == "test"
+    assert valid_track.voice == "Mary"
+    assert valid_track.languages == ["en", "en-GB"]
+    assert valid_track.classes == ["v.first.loud", "i.foreignphrase"]
+
+    with pytest.raises(ValidationError, match="end_time"):
+        ProvenanceTrack(start_time=WebVTTTimestamp(raw="00:11.000"))
+
+    with pytest.raises(ValidationError, match="should be a valid list"):
+        ProvenanceTrack(
+            start_time=WebVTTTimestamp(raw="00:11.000"),
+            end_time=WebVTTTimestamp(raw="00:12.000"),
+            languages="en",
+        )
